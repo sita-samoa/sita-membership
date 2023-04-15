497ddc265e06d131912c74e555f793686aae2b53
Andre Westerlund <andrewest_04@hotmail.com>
2023-04-15 21:16:02 +1200

    Keep password on Registration page when it errors out

--- laravel/resources/js/Pages/Auth/Register.vue
+++ laravel/resources/js/Pages/Auth/Register.vue
@@ -17,9 +17,7 @@ const form = useForm({
 });
 
 const submit = () => {
-    form.post(route('register'), {
-        onFinish: () => form.reset('password', 'password_confirmation'),
-    });
+    form.post(route('register'));
 };
 </script>
 
