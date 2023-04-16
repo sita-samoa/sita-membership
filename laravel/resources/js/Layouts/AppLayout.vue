a3d5f50f7de4c0782a9206a1944430a67aeea28b
Ainsof So'o <ainsof.soo@gmail.com>
2023-04-16 20:43:17 +1300

    display flash messages

--- laravel/resources/js/Layouts/AppLayout.vue
+++ laravel/resources/js/Layouts/AppLayout.vue
@@ -7,6 +7,7 @@ import Dropdown from '@/Components/Dropdown.vue';
 import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
+import FlashMessages from '@/Components/FlashMessages.vue'
 
 defineProps({
     title: String,
@@ -279,6 +280,8 @@ const logout = () => {
             <!-- Page Heading -->
             <header v-if="$slots.header" class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+
+                    <FlashMessages />
                     <slot name="header" />
                 </div>
             </header>
