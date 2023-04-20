9cd5bf38c2144703cc431e0bfc88c88a852c29d6
Ainsof So'o <ainsof.soo@gmail.com>
2023-04-21 11:44:40 +1300

    hide if empty

--- laravel/resources/js/Components/MemberQualificationsList.vue
+++ laravel/resources/js/Components/MemberQualificationsList.vue
@@ -5,7 +5,7 @@ import PencilOutlineIcon from 'vue-material-design-icons/PencilOutline.vue';
 defineProps(["list"])
 </script>
 <template>
-<list-group class="w-full">
+<list-group class="w-full" v-show="list.length">
   <list-group-item v-for="item in list" @click="$emit('editItem', item.id)">
     <template #prefix>
       <PencilOutlineIcon fillColor="green" />
