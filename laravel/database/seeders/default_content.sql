d5701f3b97ab4b0fafaa01ae1fea349eef4812e8
Ainsof So'o <ainsof.soo@gmail.com>
2023-04-21 16:07:55 +1300

    oopsies

--- laravel/database/seeders/default_content.sql
+++ laravel/database/seeders/default_content.sql
@@ -16,8 +16,3 @@ INSERT INTO `membership_statuses` (`id`, `code`, `title`, `created_at`, `updated
 
 -- Add Membership Application Statuses
 INSERT INTO `membership_application_statuses` (`id`, `code`, `title`, `created_at`, `updated_at`) VALUES (NULL, 'draft', 'Draft', NULL, NULL), (NULL, 'submitted', 'Submitted', NULL, NULL), (NULL, 'endorsed', 'Endorsed', NULL, NULL), (NULL, 'accepted', 'Accepted', NULL, NULL);
-
--- Add some dummy content
-INSERT INTO `members` (`id`, `user_id`, `last_name`, `first_name`, `title_id`, `dob`, `gender_id`, `membership_type_id`, `job_title`, `current_employer`, `home_address`, `home_phone`, `home_mobile`, `home_email`, `work_address`, `work_phone`, `work_mobile`, `work_email`, `other_membership`, `membership_status_id`, `note`, `membership_application_status_id`, `created_at`, `updated_at`) VALUES
-(1, 1, 'So\'o', 'Ainsof', 1, '1985-08-14', 1, 1, 'Systems Developer and Analyst', 'Secretariat of the Pacific Regional Environment Programme (SPREP)', 'Moamoa', '21139', '7761098', 'ainsof.soo@gmail.com', 'Vailima', NULL, NULL, 'ainsofs@sprep.org', 'Australian Computing Society\nDrupal Association', 1, NULL, NULL, '2023-04-17 09:23:15', '2023-04-17 09:24:28'),
-(2, 1, 'Sueseu-So\'o', 'Kalameli', NULL, '1984-08-22', 2, 2, 'Legal Advisor', 'MWTI', 'Siusega', NULL, NULL, 'ceigaden@gmail.com', 'Taufusi', NULL, NULL, 'kalameli.seuseu@mwti.gov.ws', 'Sāmoa Law Society', 1, NULL, NULL, '2023-04-17 09:25:49', '2023-04-17 09:26:51');
