7ffed0727e5988e7d375620e6016c035f802812a
Andre Westerlund <andrewest_04@hotmail.com>
2023-04-22 00:51:26 +1200

    add member referee policy

---
+++ laravel/app/Policies/MemberRefereePolicy.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\MemberReferee;
+use App\Models\User;
+use App\Models\Member;
+use Illuminate\Auth\Access\Response;
+
+class MemberRefereePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, MemberReferee $memberReferee): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, MemberReferee $memberReferee): bool
+    {
+        $member = Member::where('id', $memberReferee
+        ->member_id)->first();
+
+        return $member->user()->is($user);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, MemberReferee $memberReferee): bool
+    {
+        $member = Member::where('id', $memberReferee
+        ->member_id)->first();
+
+        return $member->user()->is($user);
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, MemberReferee $memberReferee): bool
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, MemberReferee $memberReferee): bool
+    {
+        //
+    }
+}
