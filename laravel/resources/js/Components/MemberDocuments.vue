db3043db6fcf0d39cb9b7fe89cb993e8578b9584
Ainsof So'o <ainsof.soo@gmail.com>
2023-04-22 22:48:16 +1300

    add button components for add, update, delete and cancel

--- laravel/resources/js/Components/MemberDocuments.vue
+++ laravel/resources/js/Components/MemberDocuments.vue
@@ -6,7 +6,11 @@ import InputLabel from '@/Components/InputLabel.vue';
 import InputError from '@/Components/InputError.vue';
 import MemberDocumentsList from '@/Components/MemberDocumentsList.vue';
 import DeleteConfirmationModal from '@/Components/DeleteConfirmationModal.vue';
-import DialogModal from './DialogModal.vue';
+import DialogModal from '@/Components/DialogModal.vue';
+import AddButton from '@/Components/AddButton.vue';
+import DeleteButton from '@/Components/DeleteButton.vue';
+import UpdateButton from '@/Components/UpdateButton.vue';
+import CancelButton from '@/Components/CancelButton.vue';
 
 const props = defineProps({
   member_id: Number,
@@ -132,30 +136,24 @@ function deleteItem() {
     <Input v-model="form.title" :placeholder="form.file_name ? form.file_name : 'enter your title'" label="Title" class="mb-2" />
     <InputError class="mt-2" :message="form.errors.title" />
 
-    <InputLabel for="file" value="File Upload" class="mb-4" />
-    <input v-if="canAdd" type="file" id="file" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" @input="form.file = $event.target.files[0]" />
-    <InputError class="mt-2" :message="form.errors.file" />
+    <span v-if="canAdd" >
+      <InputLabel for="file" value="File Upload" class="mb-4" />
+      <input type="file" id="file" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" @input="form.file = $event.target.files[0]" />
+      <InputError class="mt-2" :message="form.errors.file" />
 
-    <Progress v-if="form.progress" :progress="form.progress.percentage">
-      {{ form.progress.percentage }}%
-    </Progress>
+      <Progress v-if="form.progress" :progress="form.progress.percentage">
+        {{ form.progress.percentage }}%
+      </Progress>
+    </span>
 
   </template>
   <template #footer>
-      <button @click="closeModalAndResetForm" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
-        Cancel
-      </button>
+    <CancelButton @click="closeModalAndResetForm" />
 
-      <div>
-        <button v-if="canAdd" @click="submit" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Add
-        </button>
-        <button v-if="canEdit" @click="showConfirmationModal = true" type="button" class="mr-3 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-          Delete
-        </button>
-        <button v-if="canEdit" @click="update" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          Update
-        </button>
+    <div>
+      <AddButton v-if="canAdd" @click="submit" />
+      <DeleteButton v-if="canEdit" @click="showConfirmationModal = true" />
+      <UpdateButton v-if="canEdit" @click="update" />
     </div>
   </template>
 </DialogModal>
