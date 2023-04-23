061ca19a1564a20c38868903c331abc31c01b382
Ainsof So'o <ainsof.soo@gmail.com>
2023-04-23 13:51:34 +1300

    owner and permission can view members

--- laravel/app/Policies/MemberPolicy.php
+++ laravel/app/Policies/MemberPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\Models\Member;
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
@@ -21,7 +22,8 @@ class MemberPolicy
      */
     public function view(User $user, Member $member): bool
     {
-        return $member->user()->is($user);
+        $team = Team::first();
+        return $member->user()->is($user) || $user->hasTeamPermission($team, 'member:read_any');
     }
 
     /**
