fce1aaff79e825da9a4de9049774f07e4840441a
Ainsof So'o <ainsof.soo@gmail.com>
2023-04-29 20:24:36 +1300

    member seeder added

---
+++ laravel/database/seeders/MembersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Member;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MembersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Member::factory()
+            ->count(50)
+            ->create();
+    }
+}
