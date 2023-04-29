5d084164b75f54649e900ee8393cd2688ca21835
Ainsof So'o <ainsof.soo@gmail.com>
2023-04-29 15:36:31 +1300

    update test

--- laravel/tests/Feature/DefaultUserTest.php
+++ laravel/tests/Feature/DefaultUserTest.php
@@ -11,7 +11,13 @@ test('test that default user is created', function () {
     $response->assertStatus(200);
 });
 
-// test that default user is created
-// test the default user belongs to team id 1
+test('test the default user belongs to team id 1', function () {
 // test that team 1 was created
 // test that team 1 is called SITA Online
+    $this->seed(UsersTableSeeder::class);
+    $user = User::first();
+
+    expect($user->fresh()->ownedTeams)->toHaveCount(1);
+    expect($user->fresh()->ownedTeams()->latest('id')->first()->id)->toEqual(1);
+    expect($user->fresh()->ownedTeams()->latest('id')->first()->name)->toEqual('SITA Online');
+});
