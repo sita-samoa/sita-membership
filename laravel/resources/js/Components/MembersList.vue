6ea8776d0eae5e99bb4e704ac98fe06e587a67a5
Ainsof So'o <ainsof.soo@gmail.com>
2023-04-30 23:37:46 +1300

    update icon

--- laravel/resources/js/Components/MembersList.vue
+++ laravel/resources/js/Components/MembersList.vue
@@ -72,7 +72,7 @@ watch(filterStatus, (value) => {
     </list-group-item>
     <list-group-item @click="filterStatus = 4">
       <template #prefix>
-        <svg class="w-4 h-4 fill-current" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 9.5A3.5 3.5 0 005.5 13H9v2.586l-1.293-1.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 15.586V13h2.5a4.5 4.5 0 10-.616-8.958 4.002 4.002 0 10-7.753 1.977A3.5 3.5 0 002 9.5zm9 3.5H9V8a1 1 0 012 0v5z" clip-rule="evenodd"></path></svg>
+        <svg class="w-4 h-4 fill-current" fill="currentColor" xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M378 810 154 586l43-43 181 181 384-384 43 43-427 427Z"/></svg>
       </template>
       Accepted
     </list-group-item>
