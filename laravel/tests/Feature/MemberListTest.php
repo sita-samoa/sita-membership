a5106c813dd28fc9d991b89bbc5539212756ed4a
Ainsof So'o <ainsof.soo@gmail.com>
2023-04-30 23:01:39 +1300

    update tests

--- laravel/tests/Feature/MemberListTest.php
+++ laravel/tests/Feature/MemberListTest.php
@@ -1,18 +1,28 @@
 <?php
 
+use App\Models\Team;
 use App\Models\User;
 
-// test /members endpoint exists
-test('test that members list end point exists', function () {
-    // $this->actingAs($user = User::factory()->create());
-    // $response->assertStatus(403);
-    $this->actingAs($user = User::factory()->withPersonalTeam()->create());
+test('test user without role cannot access', function () {
+    $this->actingAs($user = User::factory()->create());
     $response = $this->get('/members');
-    $response->assertStatus(200);
-
+    $response->assertStatus(403);
 });
 
-// test list displayed
-// test filter works
-// test pagination works
-// test permissions work
+test('test roles can access', function (string $role) {
+    // admin can access
+    $this->actingAs($admin = User::factory()->withPersonalTeam()->create());
+    $team = Team::first();
+
+    // can access
+    $user = User::factory()->create();
+    $user->teams()->attach($team, array('role' => $role));
+    $this->actingAs($user->fresh());
+    $response = $this->get('/members');
+
+    $response->assertStatus(200);
+})->with(['editor', 'executive', 'coordinator']);
+
+// @todo test list displayed
+// @todo test filter works
+// @todo test pagination works
