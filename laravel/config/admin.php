4a0d11498089b4f59f1c4cd29b397bdd1431ae80
Ainsof So'o <ainsof.soo@gmail.com>
2023-05-01 11:31:17 +1300

    58 generate default admin user and backup admin1 user for initial access (#80)
    * set default admin user and team using env variables
    
    * update docs
    
    * add test
    
    * update test

---
+++ laravel/config/admin.php
@@ -0,0 +1,17 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default admin user
+    |--------------------------------------------------------------------------
+    |
+    | Default user will be created at project installation/deployment
+    |
+    */
+
+    'admin_name' => env('ADMIN_NAME', 'admin'),
+    'admin_email' => env('ADMIN_EMAIL', 'admin@example.com'),
+    'admin_password' =>env('ADMIN_PASSWORD', 'admin'),
+    'admin_team' =>env('ADMIN_TEAM', 'SITA Online'),
+    ];
