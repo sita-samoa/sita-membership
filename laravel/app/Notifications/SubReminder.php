f493aea8c6aa7aeffb3d518e32c0d813a83ba52f
Ainsof So'o <ainsof.soo@gmail.com>
2023-05-04 10:02:18 +1300

    send sub email using queue

--- laravel/app/Notifications/SubReminder.php
+++ laravel/app/Notifications/SubReminder.php
@@ -4,10 +4,11 @@ namespace App\Notifications;
 
 use App\Models\Member;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class SubReminder extends Notification
+class SubReminder extends Notification implements ShouldQueue
 {
     use Queueable;
 
