15af2a86e18d0695d629bc323e659e9471812fbf
Ainsof So'o <ainsof.soo@gmail.com>
2023-05-08 22:28:36 +1300

    add more tests

--- laravel/tests/Feature/SubReminderTest.php
+++ laravel/tests/Feature/SubReminderTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Member;
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Support\Facades\Queue;
 
@@ -20,4 +21,46 @@ test('test that sub reminder sent', function () {
     Queue::assertPushed(\Illuminate\Notifications\SendQueuedNotifications::class, 1);
 });
 
-// @todo test only coordinator can send a reminder
+test('test user cannot send a reminder', function () {
+    Queue::fake();
+
+    $this->actingAs($user = User::factory()->create());
+    $member = Member::factory()->for($user)->create();
+
+    $response = $this->put('/members/'.$member->id.'/send-sub-reminder');
+
+    $response->assertStatus(403);
+    Queue::assertPushed(\Illuminate\Notifications\SendQueuedNotifications::class, 0);
+});
+
+test('test other roles cannot send a reminder', function (string $role)  {
+    Queue::fake();
+
+    $this->actingAs($user = User::factory()->create());
+    $member = Member::factory()->for($user)->create();
+
+    $team = Team::first();
+    $user->teams()->attach($team, ['role' => $role]);
+
+    $response = $this->put('/members/'.$member->id.'/send-sub-reminder');
+
+    $response->assertStatus(403);
+    Queue::assertPushed(\Illuminate\Notifications\SendQueuedNotifications::class, 0);
+})->with(['editor', 'executive']);
+
+test('test coordinator can send a reminder', function () {
+    Queue::fake();
+
+    $admin = User::factory()->withPersonalTeam()->create();
+
+    $this->actingAs($user = User::factory()->create());
+    $member = Member::factory()->for($user)->create();
+
+    $team = Team::first();
+    $user->teams()->attach($team, ['role' => 'coordinator']);
+
+    $response = $this->put('/members/'.$member->id.'/send-sub-reminder');
+
+    $response->assertStatus(302);
+    Queue::assertPushed(\Illuminate\Notifications\SendQueuedNotifications::class, 1);
+});
