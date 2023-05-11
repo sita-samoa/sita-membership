bbd1a0952c9c1831e5dd82f5264ec1d50b6da924
Andre Westerlund <andrewest_04@hotmail.com>
2023-05-12 09:21:33 +1200

    apply recaptcha on register page

--- laravel/resources/js/Pages/Auth/Register.vue
+++ laravel/resources/js/Pages/Auth/Register.vue
@@ -7,6 +7,8 @@ import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
+import { useReCaptcha } from 'vue-recaptcha-v3';
+import { onMounted, onUnmounted } from 'vue';
 
 const form = useForm({
     name: '',
@@ -14,13 +16,22 @@ const form = useForm({
     password: '',
     password_confirmation: '',
     terms: false,
+    captcha_token: '',
 });
+
+const { executeRecaptcha, recaptchaLoaded } = useReCaptcha()
+const recaptcha = async () => {
+      await recaptchaLoaded()
+      form.captcha_token = await executeRecaptcha('login')
+      submit();
+}
 
 const submit = () => {
     form.post(route('register'), {
         onFinish: () => form.reset('password', 'password_confirmation'),
     });
 };
+
 </script>
 
 <template>
@@ -31,7 +42,7 @@ const submit = () => {
             <AuthenticationCardLogo />
         </template>
 
-        <form @submit.prevent="submit">
+        <form @submit.prevent="recaptcha">
             <div>
                 <InputLabel for="name" value="Name" />
                 <TextInput
--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -5,6 +5,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { VueReCaptcha } from 'vue-recaptcha-v3';
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
 
@@ -12,8 +13,10 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
+        const captchaKey = props.initialPage.props.recaptcha_site_key;
         return createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(VueReCaptcha, { siteKey: captchaKey })
             .use(ZiggyVue, Ziggy)
             .mount(el);
     },
