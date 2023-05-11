a26a4cb77949f0f1a2a8802273f111429b4863fc
Andre Westerlund <andrewest_04@hotmail.com>
2023-05-12 09:22:10 +1200

    add recaptcha config

--- laravel/config/services.php
+++ laravel/config/services.php
@@ -31,4 +31,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google_recaptcha' => [
+        'url' => 'https://www.google.com/recaptcha/api/siteverify',
+        'site_key' => env('GOOGLE_RECAPTCHA_SITE_KEY'),
+        'secret_key' => env('GOOGLE_RECAPTCHA_SECRET_KEY'),
+    ]
+
 ];
