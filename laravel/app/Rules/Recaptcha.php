2e34736dd9b721f4cf8b28a9691423217edbb009
Andre Westerlund <20700150+westerandr@users.noreply.github.com>
2023-05-16 20:42:30 +1200

    Feat/recaptcha (#93)
    * add recaptcha v3
    
    * apply recaptcha on register and login pages
    
    * add recaptcha config
    
    * add Recaptcha validation rule
    
    * pass recaptcha site key via props
    
    * validate recaptcha on user create
    
    * show recaptcha badge only on registration and login pages
    
    * update README.md to include recaptcha
    
    ---------
    
    Co-authored-by: Ainsof So'o <ainsof.soo@gmail.com>

---
+++ laravel/app/Rules/Recaptcha.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Rules;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+
+class Recaptcha implements Rule {
+  /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $endpoint = config('services.google_recaptcha');
+
+        $response = Http::asForm()->post($endpoint['url'], [
+            'secret' => $endpoint['secret_key'],
+            'response' => $value,
+        ])->json();
+
+        if(  $response['success'] && $response['score'] > 0.5) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Something went wrong. Please contact us through the SITA website.';
+    }
+}
