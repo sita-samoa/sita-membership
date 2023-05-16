f0c67998b9e2a49bf1eb88650d58ced585a469a3
Ainsof So'o <ainsof.soo@gmail.com>
2023-05-16 16:21:16 +1300

    add spacing (#96)

--- laravel/resources/js/Components/InputError.vue
+++ laravel/resources/js/Components/InputError.vue
@@ -6,7 +6,7 @@ defineProps({
 
 <template>
     <div v-show="message">
-        <p class="text-sm text-red-600">
+        <p class="text-sm text-red-600 mb-5">
             {{ message }}
         </p>
     </div>
