b2478e2ae9ae2ffaf932b1f5879c979b6616579c
Ainsof So'o <ainsof.soo@gmail.com>
2023-06-01 15:27:17 +1300

    Feat/29 as the system i want to keep reminding the user of past due sub every month for 6 months set status to expired so that an email notification is sent (#125)
    * Squashed commit of the following:
    
    commit 890c41dd740417025619a268ce5d4fd73ed1fdc4
    Author: Ainsof So'o <ainsof.soo@gmail.com>
    Date:   Sun May 28 22:24:35 2023 +1300
    
        refactor membership status id to use enum
    
    commit 0057d3ba59e6e487f4f84a71a398a954e87bd970
    Author: Ainsof So'o <ainsof.soo@gmail.com>
    Date:   Sun May 28 20:56:30 2023 +1300
    
        keep latest record only
    
    commit 45c9dc3fa326ea33fc081f5c02255c2f403fdd18
    Author: Ainsof So'o <ainsof.soo@gmail.com>
    Date:   Sun May 28 20:13:38 2023 +1300
    
        refactor
    
    commit 4cd6cd6fcba1dd341e662b50db63baecf3c5ff36
    Author: Ainsof So'o <ainsof.soo@gmail.com>
    Date:   Sun May 28 19:50:12 2023 +1300
    
        add membershipStatuses
    
    commit 152d7a1205a642548e426713fea7e32bec3df9fe
    Author: Ainsof So'o <ainsof.soo@gmail.com>
    Date:   Sun May 28 19:49:51 2023 +1300
    
        refactor
    
    commit 82042c1606e643d4d7cddad14897b2b3d9b5c10f
    Author: Ainsof So'o <ainsof.soo@gmail.com>
    Date:   Sun May 28 19:46:37 2023 +1300
    
        * update docs
    
        * update tests
    
        * ensure member status is correct
    
    commit 4d7839dc2fe27a0a877b6e9942705895213694e5
    Author: Ainsof So'o <ainsof.soo@gmail.com>
    Date:   Sun May 28 16:06:26 2023 +1300
    
        test updated
    
    commit d8a6d75b337cec595995f614a2ff98b5201b1145
    Author: Ainsof So'o <ainsof.soo@gmail.com>
    Date:   Sat May 27 17:25:24 2023 +1300
    
        get memberships expiring in
    
    * Send Past Due Sub Reminders
    
    * cater for sqlite date format in github actions

--- laravel/app/Notifications/PastDueSubReminder.php
+++ laravel/app/Notifications/PastDueSubReminder.php
@@ -3,6 +3,7 @@
 namespace App\Notifications;
 
 use App\Models\Member;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -12,10 +13,15 @@ class PastDueSubReminder extends Notification implements ShouldQueue
 {
     use Queueable;
 
+    protected function getDays() {
+        $current = Carbon::now();
+        return $current->diffInDays($this->grace_period);
+    }
+
     /**
      * Create a new notification instance.
      */
-    public function __construct(public Member $member)
+    public function __construct(public Member $member, public Carbon $grace_period)
     {
         //
     }
@@ -38,11 +44,13 @@ class PastDueSubReminder extends Notification implements ShouldQueue
         return (new MailMessage)
             ->subject("Past Due Sub Reminder")
             ->greeting("Tālofa {$this->member->user->name}!")
-            ->line('Your SITA sub is now past due.')
-            ->line('This is a reminder to pay your SITA Sub. Your
+            ->line('Your SITA sub is now due.')
+            ->line("If we dont hear back from you by
+                {$this->grace_period->toFormattedDateString()}
+                ({$this->getDays()} days), your membership
+                will expire.")
+            ->line('Please make arrangements to pay your sub. Your
                 contribution is greatly appreciated.')
-            ->line('If we dont hear back from you by [date], your membership
-                will expire and no longer be a member of SITA.')
             ->action('View details', route('members.show', $this->member->id))
             ->line('Thank you for your support!');
     }
