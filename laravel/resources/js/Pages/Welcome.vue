eb20eaa22d5e6684835f518c16299d5cb610058e
Ainsof So'o <ainsof.soo@gmail.com>
2023-06-10 16:43:51 +1300

    fix welcome page

--- laravel/resources/js/Pages/Welcome.vue
+++ laravel/resources/js/Pages/Welcome.vue
@@ -14,7 +14,7 @@ defineProps({
 
   <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
     <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-      <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
+      <Link v-if="$page.props.auth.user" :href="route('dashboard.index')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
 
       <template v-else>
         <Link :href="route('login')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</Link>
