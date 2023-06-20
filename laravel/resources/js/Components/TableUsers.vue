8196eedaac6581b1ee4674b8e52cf420d71734ba
Ainsof So'o <ainsof.soo@gmail.com>
2023-06-20 21:18:39 +1300

    display user details

--- laravel/resources/js/Components/TableUsers.vue
+++ laravel/resources/js/Components/TableUsers.vue
@@ -8,12 +8,14 @@ import TableCheckboxCell from '@/Components/TableCheckboxCell.vue'
 import BaseLevel from '@/Components/BaseLevel.vue'
 import BaseButtons from '@/Components/BaseButtons.vue'
 import BaseButton from '@/Components/BaseButton.vue'
-import UserAvatar from '@/Components/UserAvatar.vue'
 import Pagination from '@/Components/Pagination.vue'
 import PaginationCount from '@/Components/PaginationCount.vue'
 
 defineProps({
-  checkable: Boolean,
+  checkable: {
+    type: Boolean,
+    default: false,
+  },
   filters: Array,
   users: Object,
 })
@@ -21,6 +23,7 @@ defineProps({
 dayjs.extend(relativeTime)
 
 const isModalActive = ref(false)
+const selectedUser = ref({})
 
 const isModalDangerActive = ref(false)
 
@@ -45,10 +48,15 @@ const checked = (isChecked, client) => {
     checkedRows.value = remove(checkedRows.value, row => row.id === client.id)
   }
 }
+
+function showModal(user) {
+  isModalActive.value = true
+  selectedUser.value = user
+}
 </script>
 
 <template>
-  <CardBoxModal v-model="isModalActive" title="Sample modal">
+  <CardBoxModal v-model="isModalActive" :title=selectedUser.name>
     <p>Lorem ipsum dolor sit amet <b>adipiscing elit</b></p>
     <p>This is sample modal</p>
   </CardBoxModal>
@@ -67,11 +75,10 @@ const checked = (isChecked, client) => {
   <!-- No results message -->
   <PaginationCount :from="users.from" :to="users.to" :total="users.total" itemText="users" />
 
-  <table>
+  <table class="mb-3">
     <thead>
       <tr>
         <th v-if="checkable" />
-        <th />
         <th>Name</th>
         <th>Email</th>
         <th>Verified</th>
@@ -82,9 +89,6 @@ const checked = (isChecked, client) => {
     <tbody>
       <tr v-for="client in users.data" :key="client.id">
         <TableCheckboxCell v-if="checkable" @checked="checked($event, client)" />
-        <td class="border-b-0 lg:w-6 before:hidden">
-          <UserAvatar :username="client.name" class="w-24 h-24 mx-auto lg:w-6 lg:h-6" />
-        </td>
         <td data-label="Name">
           {{ client.name }}
         </td>
@@ -99,7 +103,7 @@ const checked = (isChecked, client) => {
         </td>
         <td class="before:hidden lg:w-1 whitespace-nowrap">
           <BaseButtons type="justify-start lg:justify-end" no-wrap>
-            <BaseButton color="info" :icon="mdiEye" small @click="isModalActive = true" />
+            <BaseButton color="info" :icon="mdiEye" small @click="showModal(client)" />
             <BaseButton color="danger" :icon="mdiTrashCan" small @click="isModalDangerActive = true" />
           </BaseButtons>
         </td>
@@ -108,12 +112,4 @@ const checked = (isChecked, client) => {
   </table>
 
   <Pagination :links="users.links" />
-  <!-- <div class="p-3 lg:px-6 border-t border-gray-100 dark:border-slate-800">
-    <BaseLevel>
-      <BaseButtons>
-        <BaseButton v-for="page in pagesList" :key="page" :active="page === currentPage" :label="page + 1" :color="page === currentPage ? 'lightDark' : 'whiteDark'" small @click="currentPage = page" />
-      </BaseButtons>
-      <small>Page {{ currentPageHuman }} of {{ numPages }}</small>
-    </BaseLevel>
-  </div> -->
 </template>
