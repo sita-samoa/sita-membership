898966de1ccf8e5122d4a452ec8e7b3d8faa9d3f
Ainsof So'o <ainsof.soo@gmail.com>
2023-06-26 22:02:18 +1300

    update mailing list and welcome ui

--- laravel/resources/js/Components/Welcome.vue
+++ laravel/resources/js/Components/Welcome.vue
@@ -1,11 +1,11 @@
 <script setup>
-import { Link, usePage } from '@inertiajs/vue3'
-import { Alert, Button } from 'flowbite-vue'
-import { mdiSendCheck, mdiClockOutline, mdiCurrencyUsd, mdiReload } from '@mdi/js'
-import CardBox from '@/Components/CardBox.vue'
+import { usePage } from '@inertiajs/vue3'
+import { mdiSendCheck, mdiClockOutline, mdiCurrencyUsd, mdiClipboardText } from '@mdi/js'
 import BaseButton from '@/Components/BaseButton.vue'
 import CardBoxComponentTitle from '@/Components/CardBoxComponentTitle.vue'
 import CardBoxWidget from '@/Components/CardBoxWidget.vue'
+import SectionBanner from '@/Components/SectionBanner.vue'
+import { gradientBgPinkRed } from '@/colors'
 
 defineProps({
   totalSubmitted: {
@@ -25,36 +25,26 @@ defineProps({
 
 <template>
   <!-- welcome and sign up -->
-  <div class="grid grid-cols-1 gap-6 mb-6 lg:grid-cols-2">
-    <CardBox is-hoverable>
-      <CardBoxComponentTitle :title="'Tālofa ' + usePage().props.auth.user.name">
-        <BaseButton :icon="mdiReload" color="whiteDark" rounded-full />
-      </CardBoxComponentTitle>
-      <div class="space-y-3">
-        <!-- does have a profile -->
-        <div v-if="!$page.props.user.member_id || !$page.props.user?.completion?.data?.part2?.status">
-          <Alert type="info" class="mb-2 mt-3"> Click the Sign up button to begin. </Alert>
-
-          <Link href="/members/signup">
-            <Button class="p-3 mt-3">Sign Up</Button>
-          </Link>
-        </div>
-
-        <!-- has a profile -->
-        <div v-else>
-          <Alert type="info" class="mb-2 mt-3"> Click the View details button to review your details. </Alert>
-
-          <Link :href="route('members.show', { id: $page.props.user.member_id })">
-            <Button class="p-3 mt-3">View details</Button>
-          </Link>
-        </div>
+  <div class="mb-6">
+    <CardBoxComponentTitle :title="'Tālofa, ' + usePage().props.auth.user.name" />
+    <SectionBanner v-if="!$page.props.user.member_id || !$page.props.user?.completion?.data?.part2?.status" :class="gradientBgPinkRed">
+      <h1 class="text-3xl text-white mb-6">Click the Sign up button to begin.</h1>
+      <div>
+        <BaseButton route-name="members.signup" :icon="mdiClipboardText" label="Signup" target="_blank" rounded-full />
       </div>
-    </CardBox>
+    </SectionBanner>
+
+    <SectionBanner v-else :class="gradientBgPinkRed">
+      <h1 class="text-3xl text-white mb-6">Click the Signup details button to review your details.</h1>
+      <div>
+        <BaseButton route-name="members.signup" :icon="mdiClipboardText" label="Signup details" target="_blank" rounded-full />
+      </div>
+    </SectionBanner>
   </div>
 
   <!-- exec dash -->
   <div v-if="$page.props.user.permissions.canReadAny">
-    <CardBoxComponentTitle title="Executive Dashboard" />
+    <CardBoxComponentTitle title="Members" />
 
     <div class="grid grid-cols-1 gap-6 lg:grid-cols-3 mb-6">
       <CardBoxWidget is-hoverable color="text-emerald-500" :icon="mdiSendCheck" :number="totalSubmitted" label="Pending Endorsements" href="/members?membership_status_id=2" />
