0f9d881aeb080102f77103829104ca6af5b27677
Ainsof So'o <ainsof.soo@gmail.com>
2023-06-27 01:32:33 +1300

    Feat/update dashboard (#154)
    * update executive dashboard
    
    * update mailing list and welcome ui
    
    * fix: uncomment sub date code
    
    ---------
    
    Co-authored-by: Andre Westerlund <andrewest_04@hotmail.com>

--- laravel/resources/js/Components/CardBoxWidget.vue
+++ laravel/resources/js/Components/CardBoxWidget.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { Link } from '@inertiajs/vue3'
 import { mdiCog } from '@mdi/js'
 import CardBox from '@/Components/CardBox.vue'
 import NumberDynamic from '@/Components/NumberDynamic.vue'
@@ -44,25 +45,31 @@ defineProps({
     type: Boolean,
     default: true,
   },
+  href: {
+    type: String,
+    default: '',
+  },
 })
 </script>
 
 <template>
   <CardBox>
-    <BaseLevel v-if="trend" class="mb-3" mobile>
-      <PillTagTrend :trend="trend" :trend-type="trendType" small />
-      <BaseButton v-if="showSecondaryIcon" :icon="mdiCog" icon-w="w-4" icon-h="h-4" color="lightDark" small />
-    </BaseLevel>
-    <BaseLevel mobile>
-      <div>
-        <h3 class="text-lg leading-tight text-gray-500 dark:text-slate-400">
-          {{ label }}
-        </h3>
-        <h1 class="text-3xl leading-tight font-semibold">
-          <NumberDynamic :value="number" :prefix="prefix" :suffix="suffix" />
-        </h1>
-      </div>
-      <BaseIcon v-if="icon" :path="icon" size="48" w="" h="h-16" :class="color" />
-    </BaseLevel>
+    <Link :href="href">
+      <BaseLevel v-if="trend" class="mb-3" mobile>
+        <PillTagTrend :trend="trend" :trend-type="trendType" small />
+        <BaseButton v-if="showSecondaryIcon" :icon="mdiCog" icon-w="w-4" icon-h="h-4" color="lightDark" small />
+      </BaseLevel>
+      <BaseLevel mobile>
+        <div>
+          <h3 class="text-lg leading-tight text-gray-500 dark:text-slate-400">
+            {{ label }}
+          </h3>
+          <h1 class="text-3xl leading-tight font-semibold">
+            <NumberDynamic :value="number" :prefix="prefix" :suffix="suffix" />
+          </h1>
+        </div>
+        <BaseIcon v-if="icon" :path="icon" size="48" w="" h="h-16" :class="color" />
+      </BaseLevel>
+    </Link>
   </CardBox>
 </template>
