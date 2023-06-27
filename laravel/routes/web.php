16f417d196b79ce1fe49db2f3d4f940b972ff2d2
Ainsof So'o <ainsof.soo@gmail.com>
2023-06-27 21:08:20 +1300

    update welcome page

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -29,8 +29,6 @@ Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
     ]);
 });
 
