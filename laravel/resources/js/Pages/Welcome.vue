922e6366e09d1fe48bb94bae08599bdac7801ee8
Ainsof So'o <ainsof.soo@gmail.com>
2023-06-27 20:28:51 +1300

    add dark mode switch to welcome

--- laravel/resources/js/Pages/Welcome.vue
+++ laravel/resources/js/Pages/Welcome.vue
@@ -2,6 +2,9 @@
 import { Head, Link } from '@inertiajs/vue3'
 import ApplicationMark from '@/Components/ApplicationMark.vue'
 import LayoutGuest from '@/Layouts/LayoutGuest.vue'
+import BaseIcon from '@/Components/BaseIcon.vue'
+import { useStyleStore } from '@/Stores/style.js'
+import { mdiThemeLightDark } from '@mdi/js'
 
 defineProps({
   canLogin: Boolean,
@@ -9,13 +12,17 @@ defineProps({
   laravelVersion: String,
   phpVersion: String,
 })
+
+const styleStore = useStyleStore()
 </script>
 
 <template>
   <Head title="Welcome" />
   <LayoutGuest>
     <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
+
       <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
+        <a @click="styleStore.setDarkMode()" href="#" title="Light/Dark Mode" class="mr-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"><BaseIcon :path="mdiThemeLightDark" /></a>
         <Link v-if="$page.props.auth.user" :href="route('dashboard.index')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
 
         <template v-else>
