924864a98c7bc6e637eb8373868401b345f6ca15
Ainsof So'o <ainsof.soo@gmail.com>
2023-07-08 18:39:52 +1300

    Feat/update welcome (#155)
    Co-authored-by: ainsofs <ainsofs@users.noreply.github.com>

--- laravel/resources/js/Components/MailingListDisplay.vue
+++ laravel/resources/js/Components/MailingListDisplay.vue
@@ -9,9 +9,11 @@ const props = defineProps({
 </script>
 
 <template>
-  <CardBoxComponentTitle title="Mailing List Subscriptions" />
-  <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 mb-6">
-    <CardBoxWidget is-hoverable color="text-lime-500" :icon="mdiEmailCheckOutline" :number="props.mailingLists[1][1]" label="General" href="/mailing-lists?id=1" />
-    <CardBoxWidget is-hoverable color="text-fuchsia-500" :icon="mdiEmailCheckOutline" :number="props.mailingLists[2][1]" label="Members" href="/mailing-lists?id=2" />
+  <div v-if="$page.props.user.permissions.canReadAny">
+    <CardBoxComponentTitle title="Mailing List Subscriptions" />
+    <div class="grid grid-cols-1 gap-6 lg:grid-cols-2 mb-6">
+      <CardBoxWidget is-hoverable color="text-lime-500" :icon="mdiEmailCheckOutline" :number="props.mailingLists[1][1]" label="General" href="/mailing-lists?id=1" />
+      <CardBoxWidget is-hoverable color="text-fuchsia-500" :icon="mdiEmailCheckOutline" :number="props.mailingLists[2][1]" label="Members" href="/mailing-lists?id=2" />
+    </div>
   </div>
 </template>
