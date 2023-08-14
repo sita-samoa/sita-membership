07dfcf8840143619c67e0650f52c88af0f11700f
Andre Westerlund <andrewest_04@hotmail.com>
2023-08-14 17:01:08 +1200

    add rejection notification

---
+++ laravel/app/Notifications/RejectionNotification.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Member;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class RejectionNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(public Member $member, public string $reason)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage())
+            ->subject('Signup rejected')
+            ->greeting("Tālofa {$this->member->user->name}!")
+            ->line('A signup request has been rejected. Due to the following reason:')
+            ->line($this->reason)
+            ->action('View details', route('members.show', $this->member->id));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
