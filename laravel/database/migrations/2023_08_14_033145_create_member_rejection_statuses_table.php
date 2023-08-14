5d92ffce9de523d272c92d785de223599c65a462
Andre Westerlund <andrewest_04@hotmail.com>
2023-08-14 17:00:10 +1200

    add member rejection reasons model

---
+++ laravel/database/migrations/2023_08_14_033145_create_member_rejection_statuses_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('member_rejection_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('member_id')->constrained()->cascadeOnDelete();
+            $table->string('reason')->nullable();
+            $table->boolean('status')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('member_rejection_statuses');
+    }
+};
