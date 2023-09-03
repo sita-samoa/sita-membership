00b61fd3cbd534a2ea75b45458ff5622775ed590
Andre Westerlund <andrewest_04@hotmail.com>
2023-09-03 18:13:50 +1200

    add test

---
+++ laravel/tests/Feature/InvoiceNotificationTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use App\Models\Member;
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Support\Facades\Notification;
+use App\Enums\MembershipType;
+use App\Enums\MembershipStatus;
+use App\Notifications\Invoice;
+use Carbon\Carbon;
+
+beforeEach(function () {
+    $this->seed(DatabaseSeeder::class);
+});
+
+test('test invoice notification sent', function ($membership_type_id) {
+    Notification::fake();
+    $admin = User::factory()->withPersonalTeam()->create();
+    $team = Team::first();
+
+    $this->actingAs($user = User::factory()->create());
+    $team->users()->attach(
+        $user,
+        ['role' => 'coordinator']
+    );
+
+    $member = Member::factory()->create();
+    $member->membership_status_id = MembershipStatus::ENDORSED;
+    $member->membership_type_id = $membership_type_id->value;
+    $member->save();
+
+    $response = $this->put('/members/'.$member->id.'/accept',
+        ["financial_year" => Carbon::now()->year, "receipt_number" => "121"]);
+    $response->assertStatus(302);
+
+    Notification::assertSentTo(
+        [$member->user], Invoice::class
+    );
+})->with([
+    'FULL' => MembershipType::FULL,
+    'ASSOCIATED' => MembershipType::ASSOCIATED,
+    'AFFILIATE' => MembershipType::AFFILIATE
+]);
+
+test('test invoice notification not sent', function ($membership_type_id) {
+    Notification::fake();
+    $admin = User::factory()->withPersonalTeam()->create();
+    $team = Team::first();
+
+    $this->actingAs($user = User::factory()->create());
+    $team->users()->attach(
+        $user,
+        ['role' => 'coordinator']
+    );
+
+
+    $member = Member::factory()->create();
+    $member->membership_status_id = MembershipStatus::ENDORSED;
+    $member->membership_type_id = $membership_type_id->value;
+    $member->save();
+
+    $response = $this->put('/members/'.$member->id.'/accept',
+    ["financial_year" => Carbon::now()->year, "receipt_number" => "111"]);
+    $response->assertStatus(302);
+
+    Notification::assertNothingSent();
+})->with([
+    'STUDENT' => MembershipType::STUDENT,
+    'FELLOW' => MembershipType::FELLOW
+]);
