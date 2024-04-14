c11c541fe7ebf4c932a9017b81738a561ab7c1fd
Ainsof So'o <ainsof.soo@gmail.com>
2024-04-14 17:04:11 +1300

    update seeder

--- laravel/database/seeders/MembersTableSeeder.php
+++ laravel/database/seeders/MembersTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\Member;
 use App\Models\MemberMailingPreference;
+use App\Models\MemberMembershipStatus;
+use App\Repositories\MemberRepository;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
@@ -20,6 +22,8 @@ class MembersTableSeeder extends Seeder
             ->count($members_num)
             ->create();
 
+        $rep = new MemberRepository();
+
         // add members to mailing lists
         for ($i = 0; $i < $members_num; $i++) {
             $current = Carbon::now();
@@ -31,6 +35,18 @@ class MembersTableSeeder extends Seeder
                 'created_at' => $current->toDateString(),
                 'updated_at' => $date,
             ]);
+            $from_date = Carbon::now();
+            $to_date = $rep->generateEndDate($from_date);
+
+            MemberMembershipStatus::create([
+                'member_id' => $i + 1,
+                'membership_status_id' => rand(1, 8),
+                'user_id' => 1,
+                'from_date' => $from_date,
+                'to_date' => $to_date,
+                'receipt_number' => rand(100000, 999999),
+            ]);
+
         }
     }
 }
