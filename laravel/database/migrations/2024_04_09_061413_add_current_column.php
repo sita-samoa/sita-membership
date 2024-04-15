b78fb959972d1d62ea5dafef5c87b9257004508d
Ainsof So'o <ainsof.soo@gmail.com>
2024-04-15 09:19:07 +0000

    Update 2024_04_09_061413_add_current_column.php
    add down action

--- laravel/database/migrations/2024_04_09_061413_add_current_column.php
+++ laravel/database/migrations/2024_04_09_061413_add_current_column.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('member_work_experiences', function (Blueprint $table) {
-            //
+            $table->dropColumn('is_current');
         });
     }
 };
