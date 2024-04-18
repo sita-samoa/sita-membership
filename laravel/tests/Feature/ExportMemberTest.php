b6895931c793225ca48d96947257de969b4ef0f7
Ainsof So'o <ainsof.soo@gmail.com>
2024-04-18 03:50:12 +0000

    Fix/210 member export is broken (#211)
    * add fail test
    
    * deploy fix

--- laravel/tests/Feature/ExportMemberTest.php
+++ laravel/tests/Feature/ExportMemberTest.php
@@ -1,10 +1,30 @@
 <?php
 
+use App\Models\Member;
 use App\Models\User;
+use Database\Seeders\DatabaseSeeder;
+
+beforeEach(function () {
+    $this->seed(DatabaseSeeder::class);
+});
 
 test('members exported', function () {
     $this->actingAs($user = User::factory()->withPersonalTeam()->create());
     $response = $this->get('/members/export');
 
     $response->assertStatus(200);
+    $response->assertHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    $response->assertHeader('Content-Disposition', 'attachment; filename=members.xlsx');
 });
+
+test('export member with null title_id', function () {
+    $this->actingAs($user = User::factory()->withPersonalTeam()->create());
+    $member = Member::factory()->create(['title_id' => null]);
+    $response = $this->get('/members/export');
+
+    $response->assertStatus(200);
+    $response->assertHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    $response->assertHeader('Content-Disposition', 'attachment; filename=members.xlsx');
+});
+
+
