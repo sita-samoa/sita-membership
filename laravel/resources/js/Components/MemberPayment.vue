96de9a44125304d6fe3eb5dfec324bc134cb524c
Ainsof So'o <ainsof.soo@gmail.com>
2024-06-18 11:55:42 +1300

    hide edit button

--- laravel/resources/js/Components/MemberPayment.vue
+++ laravel/resources/js/Components/MemberPayment.vue
@@ -9,16 +9,18 @@ import {
   FwbTableRow,
 } from 'flowbite-vue'
 
+const allowEdit = false
+
 defineProps(['statuses'])
 
 </script>
 <template>
-  <fwb-table>
+  <fwb-table v-if="statuses.length > 0">
     <fwb-table-head>
       <fwb-table-head-cell>From Date</fwb-table-head-cell>
       <fwb-table-head-cell>To Date</fwb-table-head-cell>
       <fwb-table-head-cell>Receipt Number</fwb-table-head-cell>
-      <fwb-table-head-cell>
+      <fwb-table-head-cell v-if="allowEdit">
         <span class="sr-only">Edit</span>
       </fwb-table-head-cell>
     </fwb-table-head>
@@ -27,7 +29,7 @@ defineProps(['statuses'])
         <fwb-table-cell>{{ c.from_date }}</fwb-table-cell>
         <fwb-table-cell>{{ c.to_date }}</fwb-table-cell>
         <fwb-table-cell>{{ c.receipt_number }}</fwb-table-cell>
-        <fwb-table-cell>
+        <fwb-table-cell v-if="allowEdit">
           <fwb-a href="#">
             Edit
           </fwb-a>
@@ -35,4 +37,5 @@ defineProps(['statuses'])
       </fwb-table-row>
     </fwb-table-body>
   </fwb-table>
+  <div v-else class="text-sm">Receipt numbers will be displayed here.</div>
 </template>
