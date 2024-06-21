fb80691d6c4fc097938cd138a16a7b12ae041aeb
Ainsof So'o <ainsof.soo@gmail.com>
2024-06-22 07:27:04 +1300

    update validation and business logic

--- laravel/app/Http/Controllers/MemberMembershipStatusController.php
+++ laravel/app/Http/Controllers/MemberMembershipStatusController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Enums\MembershipStatus;
 use App\Models\Member;
 use App\Models\MemberMembershipStatus;
+use App\Repositories\MemberRepository;
+use App\Repositories\MembershipTypeRepository;
 use Illuminate\Http\Request;
 
 class MemberMembershipStatusController extends Controller
@@ -57,16 +59,52 @@ class MemberMembershipStatusController extends Controller
         // @TODO - Check that only coordinator can update this field.
         $this->authorize('update', $member);
 
-        $validated = $request->validate([
-            'membership_status_id' => 'int',
+        $mtRep = new MembershipTypeRepository();
+        $memberships = $mtRep->getFreeMemberships();
+        $isFreeMembership = false;
 
-            // Additional validation of accepted is selected.
-            'financial_year' => $request['membership_status_id'] == MembershipStatus::ACCEPTED->value ? 'required|int|min:2000' : '',
-            'receipt_number' => $request['membership_status_id'] == MembershipStatus::ACCEPTED->value ? 'required|string' : '',
-        ]);
+        $memberRep = new MemberRepository();
 
-        $member->membership_status_id = $validated['membership_status_id'];
-        $member->save();
+        foreach ($memberships as $freeMembership) {
+            if ($member->membershipType->id == $freeMembership->id) {
+                $isFreeMembership = true;
+                break;
+            }
+        }
+
+        $isAccepted = $request['membership_status_id'] == MembershipStatus::ACCEPTED->value;
+
+        if ($isFreeMembership) {
+            $validated = $request->validate([
+                'membership_status_id' => 'int',
+
+                // Additional validation of accepted is selected.
+                'financial_year' => $isAccepted ? 'required|int|min:2000' : '',
+                'receipt_number' => $isAccepted ? 'nullable|string' : '',
+            ]);
+        }
+        else {
+            $validated = $request->validate([
+                'membership_status_id' => 'int',
+
+                // Additional validation of accepted is selected.
+                'financial_year' => $isAccepted ? 'required|int|min:2000' : '',
+                'receipt_number' => $isAccepted ? 'required|string' : '',
+            ]);
+        }
+
+        if ($isAccepted) {
+            $memberRep->accept(
+                $member,
+                $request->user(),
+                $validated['financial_year'],
+                $validated['receipt_number'] ?? ''
+            );
+        }
+        else {
+            $status = MembershipStatus::fromInt($validated['membership_status_id']);
+            $memberRep->updateMembershipStatus($member, $status);
+        }
 
         return redirect()->back()->with('success', 'Membership Status Updated');
     }
