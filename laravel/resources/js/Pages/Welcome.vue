800023b7aaea217e2352b5dfc18d25fe04435a7f
Ainsof So'o <ainsof.soo@gmail.com>
2024-06-25 07:31:43 +1300

    add search members list card to home page

--- laravel/resources/js/Pages/Welcome.vue
+++ laravel/resources/js/Pages/Welcome.vue
@@ -26,12 +26,21 @@ const styleStore = useStyleStore()
     </div>
 
     <div class="my-16">
-      <div class="grid grid-cols-1 md:grid-cols-1 gap-6 lg:gap-8">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
         <fwb-card href="#" variant="horizontal" img-src="/imgs/sita_cover.webp" img-alt="Desk">
           <div class="p-5">
             <Link :href="route('dashboard.index')">
               <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Join SITA</h5>
               <p class="font-normal text-gray-700 dark:text-gray-400">You've reached the SITA Online portal. Use it to Sign up and join the Sāmoa Information Technology Association. Register an account to get started or login to complete your Sign up.</p>
+            </Link>
+          </div>
+        </fwb-card>
+
+        <fwb-card href="#" variant="horizontal" img-src="/imgs/poly-laptop.webp" img-alt="Desk">
+          <div class="p-5">
+            <Link :href="route('members-list.index')">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Search Members List</h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">Verify the status of our members. Check their membership details, their area of interest, and their skills. Search for members by name, email, or skills.</p>
             </Link>
           </div>
         </fwb-card>
