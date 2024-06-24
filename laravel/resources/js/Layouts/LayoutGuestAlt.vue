bfee0b7197b15731d578a780b8999bfc45c72f3e
Ainsof So'o <ainsof.soo@gmail.com>
2024-06-24 21:25:40 +1300

    update nav

--- laravel/resources/js/Layouts/LayoutGuestAlt.vue
+++ laravel/resources/js/Layouts/LayoutGuestAlt.vue
@@ -20,6 +20,9 @@ const styleStore = useStyleStore()
     <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
       <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
         <a @click="styleStore.setDarkMode()" href="#" title="Light/Dark Mode" class="mr-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"><BaseIcon :path="mdiThemeLightDark" /></a>
+
+        <Link :href="route('member-list.index')" class="mr-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Member List</Link>
+
         <Link v-if="$page.props.auth.user" :href="route('dashboard.index')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
 
         <template v-else>
