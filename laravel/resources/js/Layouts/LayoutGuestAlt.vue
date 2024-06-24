a6007c70c63262bb17ed2886254092f9920caddc
Ainsof So'o <ainsof.soo@gmail.com>
2024-06-25 07:25:32 +1300

    update nav links

--- laravel/resources/js/Layouts/LayoutGuestAlt.vue
+++ laravel/resources/js/Layouts/LayoutGuestAlt.vue
@@ -5,6 +5,7 @@ import BaseIcon from '@/Components/BaseIcon.vue'
 import { useStyleStore } from '@/Stores/style.js'
 import { mdiThemeLightDark } from '@mdi/js'
 import FooterBar from '@/Components/FooterBar.vue'
+import GuestNavLink from '@/Components/GuestNavLink.vue'
 
 defineProps({
   canLogin: Boolean,
@@ -19,17 +20,17 @@ const styleStore = useStyleStore()
   <LayoutGuest>
     <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
       <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
-        <a @click="styleStore.setDarkMode()" href="#" title="Light/Dark Mode" class="mr-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"><BaseIcon :path="mdiThemeLightDark" /></a>
+        <GuestNavLink href="/" :active="route().current('/')">Home</GuestNavLink>
+        <GuestNavLink :href="route('members-list.index')" :active="route().current(route('members-list.index'))" >Members List</GuestNavLink>
 
-        <Link :href="route('members-list.index')" class="mr-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Members List</Link>
-
-        <Link v-if="$page.props.auth.user" :href="route('dashboard.index')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
+        <GuestNavLink v-if="$page.props.auth.user" :href="route('dashboard.index')">Dashboard</GuestNavLink>
 
         <template v-else>
-          <Link :href="route('login')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</Link>
+          <GuestNavLink :href="route('login')">Log in</GuestNavLink>
 
-          <Link v-if="canRegister" :href="route('register')" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</Link>
+          <GuestNavLink v-if="canRegister" :href="route('register')">Register</GuestNavLink>
         </template>
+        <a @click="styleStore.setDarkMode()" href="#" title="Light/Dark Mode" class="mr-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"><BaseIcon :path="mdiThemeLightDark" /></a>
       </div>
 
       <div class="max-w-7xl mx-auto p-6 lg:p-8">
