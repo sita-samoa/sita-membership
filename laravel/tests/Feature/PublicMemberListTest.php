69b4dda36fd40ff31c5c59bea67b2b50b3e6fa39
Ainsof So'o <ainsof.soo@gmail.com>
2024-06-26 20:55:23 +1300

    update tests

--- laravel/tests/Feature/PublicMemberListTest.php
+++ laravel/tests/Feature/PublicMemberListTest.php
@@ -7,6 +7,11 @@ use Inertia\Testing\AssertableInertia as Assert;
 
 beforeEach(function () {
     $this->seed(DatabaseSeeder::class);
+
+    // Test data.
+    Member::factory()->count(3)->create(['membership_status_id' => MembershipStatus::ACCEPTED->value]);
+    Member::factory()->count(3)->create(['membership_status_id' => MembershipStatus::LAPSED->value]);
+    Member::factory()->count(1)->create(['membership_status_id' => MembershipStatus::DRAFT->value]);
 });
 
 test('test members list exists', function () {
@@ -20,18 +25,57 @@ test('test can filter by member_status_id and search', function () {
     $status = MembershipStatus::ACCEPTED->value;
     $search = 'test';
 
-    Member::factory()->count(3)->create(['membership_status_id' => MembershipStatus::ACCEPTED->value]);
-    Member::factory()->count(1)->create(['membership_status_id' => MembershipStatus::DRAFT->value]);
+    Member::factory()->count(1)->create([
+        'membership_status_id' => MembershipStatus::ACCEPTED->value,
+        'first_name' => $search,
+    ]);
 
-    $response = $this->get('/members-list')
+    // Get only accepted members
+    $response = $this->get('/members-list?search=&membership_status_id=' . $status)
         ->assertInertia(fn (Assert $page) => $page
             ->component('MembersList/Index')
-            ->has('members.data', 3) // Ensure 10 members per page
+            ->has('members.data', 4)
+            ->where('filters.search', null)
+            ->where('filters.membership_status_id', strval($status))
+        );
+
+    // Get only lapsed members
+    $lapsed = MembershipStatus::LAPSED->value;
+    $response = $this->get('/members-list?search=&membership_status_id=' . $lapsed)
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('MembersList/Index')
+            ->has('members.data', 3)
+            ->where('filters.search', null)
+            ->where('filters.membership_status_id', strval($lapsed))
+        );
+
+    // Get accepted members with search
+    $response = $this->get('/members-list?search=' . $search . '&membership_status_id=' . $status)
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('MembersList/Index')
+            ->has('members.data', 1)
             ->has('members.links')
             ->where('members.per_page', 10)
-            // ->where('filters.search', 'Test')
+            ->where('filters.search', $search)
+            ->where('filters.membership_status_id', strval($status))
         );
 
     $response->assertStatus(200);
 
 });
+
+test('test invalid searches', function ($status) {
+    // You can only search for Accepted and Lapsed members. All other types are invalid.
+    $search = '';
+
+    $response = $this->get('/members-list?search=' . $search . '&membership_status_id=' . $status);
+    $response->assertInvalid();
+})->with([
+    'hello',
+    MembershipStatus::DRAFT->value,
+    MembershipStatus::SUBMITTED->value,
+    MembershipStatus::ENDORSED->value,
+    MembershipStatus::EXPIRED->value,
+    MembershipStatus::BANNED->value,
+    MembershipStatus::REJECTED->value,
+]);
