267bdf2ecc855337968507a45a66bf5e53b3ec79
Ainsof So'o <ainsof.soo@gmail.com>
2024-08-05 09:24:52 +0000

    add gitpod config

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -19,8 +19,18 @@ export default defineConfig({
   ],
   server: {
     host: true,
-    hmr: {
-      host: 'localhost',
-    },
+    // hmr: {
+    //   host: 'localhost',
+    // },
+		hmr: process.env.GITPOD_WORKSPACE_URL
+			? {
+					protocol: 'wss',
+					clientPort: 443,
+					host: process.env.GITPOD_WORKSPACE_URL.replace(
+						'https://',
+						'5173-',
+					),
+			  }
+			: true,
   },
 })
